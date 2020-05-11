673100d9c3e7731fb593121358c975f19a455e8e
GersonDantas <gerson.accari@hotmail.com>
2020-05-11 08:14:19 -0300

    install package knex

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
+    console.log('func 1')
     res.status(200).send('Meu Backend')
+    next()
+})
+app.get('/', (req, res, next) => {
+    console.log('func 2')
+    res.status(200).send('Meu Backend2')
 })
 
 app.listen(3000,() =>{
